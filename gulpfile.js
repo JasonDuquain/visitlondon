c7293cbe61b7dd56e64ef21097dce67ad1cacea0
DUQUAIN <JASON.DUQUAIN@syf.com>
2017-01-11 21:13:27 -0500

    copy task in the gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -17,6 +17,7 @@ var htmlmin = require('gulp-htmlmin');
 
 var SOURCEPATHS = {
   sassSource : 'src/scss/*.scss',
+  sassApp : 'src/scss/app.scss', 
   htmlSource : 'src/*.html',
   htmlPartialSource : 'src/partial/*.html',
   jsSource : 'src/js/**',
@@ -49,7 +50,7 @@ gulp.task('sass', function(){
   var sassFiles;
   
   
-  sassFiles = gulp.src(SOURCEPATHS.sassSource)
+  sassFiles = gulp.src(SOURCEPATHS.sassApp)
       .pipe(autoprefixer())
       .pipe(sass({outputStyle: 'expanded'}).on('error', sass.logError))
       return merge(bootstrapCSS, sassFiles)
